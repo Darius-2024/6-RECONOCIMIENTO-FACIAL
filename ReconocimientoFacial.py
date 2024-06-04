ee6aff74d6a85cc6483dc33c500fddb0d046edde
daniel4dario <daniel_4dario@hotmail.com>
2024-06-04 10:06:00 -0400

    fase 2

--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -1,15 +1,16 @@
+from flask import Flask, render_template, Response
 import cv2
-import os
 import numpy as np
 from keras.models import load_model
+import datetime
+import requests
+import os
 import conexion
 
-# Obtener una referencia al bucket de almacenamiento
-bucket = conexion.initialize_firestore()
+app = Flask(__name__)
 
-# Descargar el modelo desde Firebase Storage
-blob = bucket.blob('modeloCNN.h5')
-blob.download_to_filename('modeloCNN.h5')
+# Función de inicialización de Firebase
+bucket = conexion.initialize_firestore()
 
 # Cargar el modelo entrenado
 model = load_model('modeloCNN.h5')
@@ -18,39 +19,73 @@ dataPath = 'G:/IA/OmesTutorials2020-master/6 RECONOCIMIENTO FACIAL/faces'
 peopleList = os.listdir(dataPath)
 print('Lista de personas: ', peopleList)
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture('videos/Edson.mp4')
-
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-while True:
-    ret, frame = cap.read()
-    if ret == False: 
-        break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        rostro = gray[y:y + h, x:x + w]
-        rostro = cv2.resize(rostro, (100, 100))  # Redimensionar a la misma dimensión que usamos en el entrenamiento
-        rostro = np.expand_dims(rostro, axis=0)
-        rostro = np.expand_dims(rostro, axis=3)
-        resultado = model.predict(rostro)
-
-        # Obtener el nombre de la persona
-        persona = peopleList[np.argmax(resultado)]
-
-        if resultado[0][np.argmax(resultado)] > resultado[0][1 - np.argmax(resultado)]:
-            cv2.putText(frame, 'Buscado: ' + persona, (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+def gen_frames():  # Generador de fotogramas para la transmisión de video
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    start_time = datetime.datetime.now()
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
         else:
-            cv2.putText(frame, 'No Buscado', (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                rostro = gray[y:y + h, x:x + w]
+                rostro = cv2.resize(rostro, (100, 100))  # Redimensionar a la misma dimensión que usamos en el entrenamiento
+                rostro = np.expand_dims(rostro, axis=0)
+                rostro = np.expand_dims(rostro, axis=3)
+                resultado = model.predict(rostro)
 
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1)
-    if k == 27:
-        break
+                # Obtener el nombre de la persona
+                persona = peopleList[np.argmax(resultado)]
 
-cap.release()
-cv2.destroyAllWindows()
+                if resultado[0][np.argmax(resultado)] > resultado[0][1 - np.argmax(resultado)]:
+                    cv2.putText(frame, 'Buscado: ' + persona, (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+                    # Separar la fecha y la hora
+                    fecha_hora_actual = datetime.datetime.now()
+                    # Almacenar el nombre de la persona y la hora en Firestore cada 5 segundos
+                    if fecha_hora_actual - start_time >= datetime.timedelta(seconds=5):
+                        data = {
+                            "persona": persona,
+                            "fecha_hora": fecha_hora_actual.strftime("%Y-%m-%d %H:%M:%S")  # Guardar fecha y hora combinadas
+                        }
+                        write_to_firestore(data)
+                        start_time = fecha_hora_actual
+                else:
+                    cv2.putText(frame, 'No Buscado', (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Transmite el fotograma como imagen JPEG
+
+    cap.release()
+
+# Configuración de la base de datos de Firebase Realtime
+BasePath = "https://deteccionfacial-3746c-default-rtdb.firebaseio.com/"
+AuthSecret = "ezT2u0M4G8nDKEcNIb3Hmtg3qzZhDcoWRXSsWzFj"
+
+
+def write_to_firestore(data):
+    endpoint = BasePath + "reconocimientos.json?auth=" + AuthSecret
+    response = requests.post(endpoint, json=data)
+    if response.status_code == 200:
+        print("Datos escritos en la base de datos correctamente.")
+    else:
+        print("Error al escribir datos en la base de datos:", response.text)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- conexion.py
+++ conexion.py
@@ -1,7 +1,9 @@
 import firebase_admin
-from firebase_admin import credentials, storage
+from firebase_admin import credentials, storage, initialize_app, firestore
+
 
 def initialize_firestore():
     cred = credentials.Certificate("credentials/deteccionfacial-3746c-firebase-adminsdk-byuv3-cffbb6349a.json")
     firebase_admin.initialize_app(cred, {'storageBucket': 'deteccionfacial-3746c.appspot.com'})
     return storage.bucket()
+
